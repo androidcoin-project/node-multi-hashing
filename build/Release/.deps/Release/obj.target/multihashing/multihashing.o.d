ce1fe5fd1b8be6c44d58c28db72c44f648d4e669
androidcoin-project <winstonebobby@gmail.com>
2018-02-07 11:00:19 +0700

    first commit

---
+++ build/Release/.deps/Release/obj.target/multihashing/multihashing.o.d
@@ -0,0 +1,59 @@
+cmd_Release/obj.target/multihashing/multihashing.o := g++ '-DNODE_GYP_MODULE_NAME=multihashing' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/androidcoin/.node-gyp/0.10.25/include/node -I/home/androidcoin/.node-gyp/0.10.25/src -I/home/androidcoin/.node-gyp/0.10.25/deps/uv/include -I/home/androidcoin/.node-gyp/0.10.25/deps/v8/include -I../crypto  -fPIC -Wall -Wextra -Wno-unused-parameter -pthread -m64 -D_GNU_SOURCE -maes -fPIC -Ofast -flto -fuse-linker-plugin -funroll-loops -funswitch-loops -fpeel-loops -fno-rtti -fno-exceptions -std=c++0x -maes -march=native -MMD -MF ./Release/.deps/Release/obj.target/multihashing/multihashing.o.d.raw   -c -o Release/obj.target/multihashing/multihashing.o ../multihashing.cc
+Release/obj.target/multihashing/multihashing.o: ../multihashing.cc \
+ /home/androidcoin/.node-gyp/0.10.25/src/node.h \
+ /home/androidcoin/.node-gyp/0.10.25/deps/uv/include/uv.h \
+ /home/androidcoin/.node-gyp/0.10.25/deps/uv/include/uv-private/uv-unix.h \
+ /home/androidcoin/.node-gyp/0.10.25/deps/uv/include/uv-private/ngx-queue.h \
+ /home/androidcoin/.node-gyp/0.10.25/deps/uv/include/uv-private/uv-linux.h \
+ /home/androidcoin/.node-gyp/0.10.25/deps/v8/include/v8.h \
+ /home/androidcoin/.node-gyp/0.10.25/deps/v8/include/v8stdint.h \
+ /home/androidcoin/.node-gyp/0.10.25/src/node_object_wrap.h \
+ /home/androidcoin/.node-gyp/0.10.25/src/node.h \
+ /home/androidcoin/.node-gyp/0.10.25/src/node_buffer.h ../bcrypt.h \
+ ../keccak.h ../quark.h ../scryptjane.h ../scryptn.h \
+ ../yescrypt/yescrypt.h ../yescrypt/sha256_Y.h ../neoscrypt.h ../skein.h \
+ ../x11.h ../groestl.h ../blake.h ../fugue.h ../qubit.h ../s3.h \
+ ../hefty1.h ../shavite3.h ../cryptonight.h ../x13.h ../x14.h ../nist5.h \
+ ../sha1.h ../x15.h ../fresh.h ../dcrypt.h ../jh.h ../x5.h ../c11.h \
+ ../a5a.h ../boolberry.h
+../multihashing.cc:
+/home/androidcoin/.node-gyp/0.10.25/src/node.h:
+/home/androidcoin/.node-gyp/0.10.25/deps/uv/include/uv.h:
+/home/androidcoin/.node-gyp/0.10.25/deps/uv/include/uv-private/uv-unix.h:
+/home/androidcoin/.node-gyp/0.10.25/deps/uv/include/uv-private/ngx-queue.h:
+/home/androidcoin/.node-gyp/0.10.25/deps/uv/include/uv-private/uv-linux.h:
+/home/androidcoin/.node-gyp/0.10.25/deps/v8/include/v8.h:
+/home/androidcoin/.node-gyp/0.10.25/deps/v8/include/v8stdint.h:
+/home/androidcoin/.node-gyp/0.10.25/src/node_object_wrap.h:
+/home/androidcoin/.node-gyp/0.10.25/src/node.h:
+/home/androidcoin/.node-gyp/0.10.25/src/node_buffer.h:
+../bcrypt.h:
+../keccak.h:
+../quark.h:
+../scryptjane.h:
+../scryptn.h:
+../yescrypt/yescrypt.h:
+../yescrypt/sha256_Y.h:
+../neoscrypt.h:
+../skein.h:
+../x11.h:
+../groestl.h:
+../blake.h:
+../fugue.h:
+../qubit.h:
+../s3.h:
+../hefty1.h:
+../shavite3.h:
+../cryptonight.h:
+../x13.h:
+../x14.h:
+../nist5.h:
+../sha1.h:
+../x15.h:
+../fresh.h:
+../dcrypt.h:
+../jh.h:
+../x5.h:
+../c11.h:
+../a5a.h:
+../boolberry.h:
